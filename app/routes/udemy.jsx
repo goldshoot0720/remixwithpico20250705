b8a0ffecb280aba5ef8e3813810ee4d92f353cb4
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-18 22:28:52 +0800

    remixwithpico20250705 demo

--- app/routes/udemy.jsx
+++ app/routes/udemy.jsx
@@ -2,21 +2,7 @@ export default function UdemyPage() {
   return (
     <div>
       <h1>Udemy Page</h1>
-      <h2>Tailwind CSS – The Practical Bootcamp in 2025</h2>
-      <h3>Muslim Helalee</h3>
-      <h4>
-        12/241={(12 / 241).toFixed(2) * 100}%→{12 - 241}
-        <h2>The Ultimate React Course 2025: React, Next.js, Redux & More</h2>
-        <h3>Jonas Schmedtmann</h3>
-      </h4>
-      <h4>
-        81/505={(81 / 505).toFixed(2) * 100}%→{81 - 505}
-      </h4>
-      <h2>Remix v2 complete masterclass - Build Full-stack AI apps</h2>
-      <h3>Soumya Sahu</h3>
-      <h4>
-        33/81={(33 / 81).toFixed(2) * 100}%→{33 - 81}
-      </h4>
+      <form method="POST"></form>
     </div>
   );
 }
