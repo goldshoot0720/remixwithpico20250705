7a4243d080a99bdde0d2cf5aa6dca46f09f04466
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-18 15:35:50 +0800

    Initial commit from create-remix

---
+++ remixwithpico20250705-git.bat
@@ -0,0 +1,13 @@
+git init
+
+git add .
+
+git commit -m "myv2remix07052025 MyNavBar"
+
+git remote add origin https://github.com/goldshoot0720/myv2remix07052025.git
+
+git checkout -b master
+
+git push -u origin master --force
+
+pause
---
+++ remixwithpico20250705-open.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\chbon\Documents\20250705\myv2remix07052025"
+npm run dev -- --open
+pause
